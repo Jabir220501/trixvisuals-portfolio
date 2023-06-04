ace6ab8fec0fb393f79c8ed9e63d1b2e0a2a9660
Jabir <220501@student.glu.nl>
2023-06-04 21:30:16 +0200

    slider styling halfway through

--- web-development.php
+++ web-development.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trix Visuals | Choose your category</title>
+    <title>Trix Visuals | Web Development</title>
     <link rel="stylesheet" href="./src/css/global.css">
     <link rel="stylesheet" href="./src/css/web-development.css">
 
@@ -37,8 +37,8 @@
         </header>
     </section>
     <section class="main_content">
-        <div id="showcase_slider">
-            <div class="showcase_slider_item">
+        <ul id="showcase_slider">
+            <li class="showcase_slider_item">
                 <img src="http://via.placeholder.com/865x435" alt="">
                 <div class="showcase_slider_item_hover">
                     <a href="#">LIVE PREVIEW</a>
@@ -48,8 +48,8 @@
                     <h2 class="Title">Greenly Landing Page</h2>
                     <h6 class="webdev_category">Full Stack Project</h6>
                 </div>
-            </div>
-            <div class="showcase_slider_item active">
+            </li>
+            <li class="showcase_slider_item active">
                 <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
                     alt="">
                 <div class="showcase_slider_item_hover">
@@ -60,8 +60,8 @@
                     <h2 class="Title">Greenly Landing Page</h2>
                     <h6 class="webdev_category">Full Stack Project</h6>
                 </div>
-            </div>
-            <div class="showcase_slider_item">
+            </li>
+            <li class="showcase_slider_item">
                 <img src="http://via.placeholder.com/865x435" alt="">
                 <div class="showcase_slider_item_hover">
                     <a href="#">LIVE PREVIEW</a>
@@ -71,14 +71,14 @@
                     <h2 class="Title">Greenly Landing Page</h2>
                     <h6 class="webdev_category">Full Stack Project</h6>
                 </div>
-            </div>
-        </div>
+            </li>
+        </ul>
         <div id="showcase_slider_nav">
             <div class="showcase_slider_nav_prev">
             </div>
             <div id="showcase_slider_nav_indicator">
                 <div class="showcase_slider_nav_dot"></div>
-                <div class="showcase_slider_nav_dot active">aa</div>
+                <div class="showcase_slider_nav_dot active"></div>
                 <div class="showcase_slider_nav_dot"></div>
             </div>
             <div class="showcase_slider_nav_next"></div>
